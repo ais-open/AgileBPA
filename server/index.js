0b3282aaeea591326f401b15c4619f8c0bbbfaba
Kyle Linden <Kyle.Linden@appliedis.com>
2015-06-17 15:43:36 -0400

    refactored server code to move logic out of index.js

--- server/index.js
+++ server/index.js
@@ -4,13 +4,7 @@ var Hapi = require('hapi');
 var server = new Hapi.Server();
 server.connection({port: parseInt(process.env.PORT, 10) || 3000});
 
-server.route({
-    method: 'GET',
-    path: '/api',
-    handler: function(request, reply) {
-        reply({ 'api' : 'hello!' });
-    }
-});
+server.route(require('./routes'));
 
 server.start(function() {
     console.log('Node/Hapi running on port ' + server.info.port);
