1d81a8a10016e5384ab7cc18e868a009f2bb1dcf
Michael Bowman <bowmanmc@gmail.com>
2015-06-23 11:20:35 -0400

    user profile

---
+++ ui/app/modules/components/profile/18fProfileDirective.js
@@ -0,0 +1,13 @@
+/**
+ * Usage:
+ *      <ais-profile />
+ */
+angular.module('18f').directive('aisProfile', function() {
+    'use strict';
+
+    return {
+        restrict: 'EA',
+        templateUrl: 'modules/components/profile/18fProfileTemplate.html',
+        controller: '18fProfileController'
+    };
+});
