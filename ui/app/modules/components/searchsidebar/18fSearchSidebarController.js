34164c1bdd590ac910ed39d8650ca0e492657299
Chris Dickman <chrisdickman@Chriss-MacBook-Pro.local>
2015-06-23 16:46:18 -0400

    started Add Medication on search results, getting error on post to api

--- ui/app/modules/components/searchsidebar/18fSearchSidebarController.js
+++ ui/app/modules/components/searchsidebar/18fSearchSidebarController.js
@@ -1,30 +1,39 @@
-angular.module('18f').controller('18fSearchSidebarController', function($scope,SearchService,SignInService) {
+angular.module('18f').controller('18fSearchSidebarController', function($scope,SearchService,SignInService,ProfileService) {
     'use strict';
     var initialize = function(){
         $scope.showResults = false;
         $scope.showSearching = false;
         $scope.term = '';
+        $scope.drug = null;
         $scope.results = [];
-        $scope.loggedIn = false;
-        $scope.user = SignInService.getProfile();
-        if($scope.user !== null){
-            $scope.loggedIn = true;
-        }
     };
 
     $scope.search = function(){
+        $scope.drug = null;
         $scope.showSearching = true;
         $scope.showResults = false;
         $scope.results = SearchService.search({ term: $scope.searchTerm },function(){
+            debugger;
             $scope.showSearching = false;
             $scope.showResults = true;
         },function(){
-
+            console.log('Error searching.');
         });
     };
 
     $scope.addUserDrug = function(drug){
         console.log(drug.fdaId);
+        debugger;
+        if($scope.profile){
+            ProfileService.addUserDrug({ user: $scope.profile.token },function(){
+                $scope.drug = drug;
+                $scope.showSearching = false;
+                $scope.showResults = false;
+            },function(){
+                console.log('Error searching.');
+            });
+        }
+
     };
 
     initialize();
--- ui/app/modules/services/18fProfileService.js
+++ ui/app/modules/services/18fProfileService.js
@@ -2,5 +2,7 @@ angular.module('18f').factory('ProfileService', function($resource, appConfig) {
     var url = appConfig.api + 'user/:user';
     return $resource(url, {
         user: '@user'
+    },{
+        'addUserDrug': { method: 'POST', url: url + '/drug' }
     });
 });
