75df0899d29a5e4b0b8cf0bb84c24111780feba8
Kyle Linden <Kyle.Linden@appliedis.com>
2015-06-24 16:40:26 -0400

    optimization for users without any drugs, prevents call to FDA API

--- server/services/userService.js
+++ server/services/userService.js
@@ -12,71 +12,76 @@ function UserService(UserModel) {
 
     addFdaDrugInfo = function(userDoc, callback) {
         var user = userDoc.toObject();
-        // go lookup drugs in the FDA API
-        var fdaIds = _.pluck(user.drugs, 'fdaId').join('+id:');
 
-        // call callback after all AJAX calls are complete (1 for each drug plus 1 more to get all drug details)
-        var done = _.after(user.drugs.length + 1, function() {
-            callback(user);
-        })
+        // if there are no drugs execute callback immediately and no further processing is necessary
+        if(user.drugs.length) {
+            // go lookup drugs in the FDA API
+            var fdaIds = _.pluck(user.drugs, 'fdaId').join('+id:');
 
-        // call the API to get extra drug information
-        var apiUrl = config.fdaApi.baseUrl + '/drug/label.json?api_key=' + config.fdaApi.apiKey + '&search=id:' + fdaIds + '&limit=' + user.drugs.length;
-        request(apiUrl, function(error, response, bodyRaw) {
-            var body = JSON.parse(bodyRaw);
-            _.each(body.results, function(result) {
-                // for each result returned from the API, go find that record in the original results and "hydrate" the drug information
-                var match = _.find(user.drugs, { 'fdaId' : result.id });
-                match.activeIngredients = result.active_ingredient;
-                match.brandName = result.openfda.brand_name;
-                match.genericName = result.openfda.generic_name;
-                match.manufacturerName = result.openfda.manufacturer_name;
-                match.inactiveIngredients = result.inactive_ingredient;
-                match.dosage = result.dosage_and_administration;
-                match.usage = result.indications_and_usage;
-                match.purpose = result.purpose;
-                match.warnings = result.warnings;
-                match.whenUsing = result.when_using;
-                match.drugInteractions = result.drug_interactions;
-            });
+            // call callback after all AJAX calls are complete (1 for each drug plus 1 more to get all drug details)
+            var done = _.after(user.drugs.length + 1, function() {
+                callback(user);
+            })
 
-            done();
-        });
-
-        // call the API to get drug adverse events
-        //https://api.fda.gov/drug/event.json?search=patient.drug.medicinalproduct:metformin+AND+patient.drug.drugcharacterization:1&count=patient.reaction.reactionoutcome
-        _.each(user.drugs, function(drug) {
-            var adverseEventsApiUrl = config.fdaApi.baseUrl + '/drug/event.json?api_key=' + config.fdaApi.apiKey + '&search=patient.drug.openfda.spl_id:' + drug.fdaId + '+AND+patient.drug.drugcharacterization:1&count=patient.reaction.reactionoutcome';
-            request(adverseEventsApiUrl, function(error, reponse, bodyRaw) {
+            // call the API to get extra drug information
+            var apiUrl = config.fdaApi.baseUrl + '/drug/label.json?api_key=' + config.fdaApi.apiKey + '&search=id:' + fdaIds + '&limit=' + user.drugs.length;
+            request(apiUrl, function(error, response, bodyRaw) {
                 var body = JSON.parse(bodyRaw);
-                var adverseEvents = _.map(body.results, function(event) {
-                    switch(event.term) {
-                        case 1:
-                            event.display = "Recovered/Resolved";
-                            break;
-                        case 2:
-                            event.display = "Recovering/Resolving";
-                            break;
-                        case 3:
-                            event.display = "Not Recovered/Not Resolved";
-                            break;
-                        case 4:
-                            event.display = "Recovered/Resolved with Chronic Condition";
-                            break;
-                        case 5:
-                            event.display = "Fatal";
-                            break;
-                        case 6:
-                            event.display = "Unknown";
-                            break;
-                    }
-                    return event;
-                })
-                drug.adverseEvents = adverseEvents;
+                _.each(body.results, function(result) {
+                    // for each result returned from the API, go find that record in the original results and "hydrate" the drug information
+                    var match = _.find(user.drugs, { 'fdaId' : result.id });
+                    match.activeIngredients = result.active_ingredient;
+                    match.brandName = result.openfda.brand_name;
+                    match.genericName = result.openfda.generic_name;
+                    match.manufacturerName = result.openfda.manufacturer_name;
+                    match.inactiveIngredients = result.inactive_ingredient;
+                    match.dosage = result.dosage_and_administration;
+                    match.usage = result.indications_and_usage;
+                    match.purpose = result.purpose;
+                    match.warnings = result.warnings;
+                    match.whenUsing = result.when_using;
+                    match.drugInteractions = result.drug_interactions;
+                });
+
                 done();
             });
-        })
 
+            // call the API to get drug adverse events
+            //https://api.fda.gov/drug/event.json?search=patient.drug.medicinalproduct:metformin+AND+patient.drug.drugcharacterization:1&count=patient.reaction.reactionoutcome
+            _.each(user.drugs, function(drug) {
+                var adverseEventsApiUrl = config.fdaApi.baseUrl + '/drug/event.json?api_key=' + config.fdaApi.apiKey + '&search=patient.drug.openfda.spl_id:' + drug.fdaId + '+AND+patient.drug.drugcharacterization:1&count=patient.reaction.reactionoutcome';
+                request(adverseEventsApiUrl, function(error, reponse, bodyRaw) {
+                    var body = JSON.parse(bodyRaw);
+                    var adverseEvents = _.map(body.results, function(event) {
+                        switch(event.term) {
+                            case 1:
+                                event.display = "Recovered/Resolved";
+                                break;
+                            case 2:
+                                event.display = "Recovering/Resolving";
+                                break;
+                            case 3:
+                                event.display = "Not Recovered/Not Resolved";
+                                break;
+                            case 4:
+                                event.display = "Recovered/Resolved with Chronic Condition";
+                                break;
+                            case 5:
+                                event.display = "Fatal";
+                                break;
+                            case 6:
+                                event.display = "Unknown";
+                                break;
+                        }
+                        return event;
+                    })
+                    drug.adverseEvents = adverseEvents;
+                    done();
+                });
+            });
+        } else {
+            callback(user);
+        }
     };
 
     pub.getUserByToken = function(token, callback) {
