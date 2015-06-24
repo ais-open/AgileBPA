28d2383b20aef1d5d006b1c6835d50b29e7773a7
Kyle Linden <Kyle.Linden@appliedis.com>
2015-06-24 16:41:42 -0400

    add server tests to travis.yml

--- server/test/services/userService.spec.js
+++ server/test/services/userService.spec.js
@@ -5,8 +5,8 @@ var mockgoose = require('mockgoose');
 mockgoose(mongoose);
 mongoose.connect(''); // fake a connection, mockgoose intercepts this and sets up an in-memory database
 
-var userModel = require('../../models/user')();
-var userService = require('../../services/userService')(userModel);
+var UserModel = require('../../models/user')();
+var userService = require('../../services/userService')(UserModel);
 
 var _ = require('lodash');
 var should = require('should');
@@ -17,8 +17,9 @@ describe('UserService', function() {
     beforeEach(function(done) {
         // Arrange
         mockgoose.reset();
-        userFixture = new userModel(require('../fixtures/user.json'));
-        userFixture.save(done);
+        userFixture = require('../fixtures/user.json');
+        var fixtureModel = new UserModel(userFixture);
+        fixtureModel.save(done);
     });
 
     it('should retrieve a stored user', function(done) {
@@ -28,28 +29,37 @@ describe('UserService', function() {
             data.firstName.should.equal('Joe');
             data.lastName.should.equal('User');
             data.email.should.equal('foo@bar.com');
-            data.password.should.equal('p@ssw0rd');
             data.token.should.equal('12345');
 
             done();
         })
     });
 
-    it('should add a drug to a user\'s cabinet', function(done) {
-        // Act
-        var drugToAdd = {
-            fdaId: '750fd441-8432-4b6d-b48d-5db566a90e3f',
-            userComments: 'For headaches'
-        };
-        userService.addUserDrug(userFixture.token, drugToAdd, function(err) {
-            // Assert
-            err.should.not.be.ok;
+    it('should authenticate a user', function(done) {
+        userService.authenticateUser(userFixture.email, userFixture.passwordUnencrypted, function(userDoc) {
+            userDoc.should.be.ok;
+            done();
+        });
+    });
 
-            userService.getUserByToken(userFixture.token, function(userDoc) {
-                _.find(userDoc.drugs, { 'fdaId' : drugToAdd.fdaId }).should.be.ok;
+    it('should add a user', function(done) {
+        // Act
+        var userToAdd = {
+            firstName: 'John',
+            lastName: 'Deere',
+            email: 'John@Deere.com',
+            password: 's3cr3t'
+        };
+
+        userService.addUser(userToAdd, function(err, user) {
+            // Assert
+            userService.getUserByToken(user.token, function(retrievedUser) {
+                retrievedUser.firstName.should.equal('John');
+                retrievedUser.lastName.should.equal('Deere');
+                retrievedUser.email.should.equal('John@Deere.com');
                 done();
             });
         });
-    })
+    });
 
 });
