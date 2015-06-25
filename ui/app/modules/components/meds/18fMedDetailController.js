44b1386d3d98098e6701cdcced8935439b3edb0d
Chris Dickman <chris.dickman@appliedis.com>
2015-06-25 11:00:24 -0400

    Updated Med List and Med Detail views

--- ui/app/modules/components/meds/18fMedDetailController.js
+++ ui/app/modules/components/meds/18fMedDetailController.js
@@ -1,8 +1,20 @@
-angular.module('18f').controller('18fMedDetailController', function($scope, SignInService) {
+angular.module('18f').controller('18fMedDetailController', function($scope, $routeParams, SignInService ) {
     'use strict';
 
     var initialize = function() {
         $scope.SignInService = SignInService;
+
+        $scope.$watch('SignInService.user', function() {
+            if (SignInService.user == null){
+                $scope.selectedDrug = null;
+                return;
+            }
+
+            $scope.selectedDrug = _.find(SignInService.user.drugs, { 'fdaId' : $routeParams.fdaId });
+            if(!$scope.selectedDrug){
+                swal({   title: "Error!", text: 'This drug is not associated with your profile.', type: "error", confirmButtonText: "Ok" });
+            }
+        });
     };
     initialize();
 
--- ui/app/modules/pages/meds/18fMedsDetailPageController.js
+++ ui/app/modules/pages/meds/18fMedsDetailPageController.js
@@ -1,14 +1,5 @@
 angular.module('18f').controller('18fMedsDetailPageController', function($scope) {
     'use strict';
 
-    $scope.selectedDrug = null;
-
-    $scope.selectDrug = function(drug){
-        $scope.selectedDrug = drug;
-    };
-
-    $scope.unselectDrug = function(){
-        $scope.selectedDrug = null;
-    }
 
 });
