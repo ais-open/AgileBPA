4ca7a7142d4a37f5cd5017c8edbac9539058e4fe
Michael Bowman <bowmanmc@gmail.com>
2015-06-25 14:41:08 -0400

    Hook up the view details button

--- ui/app/modules/components/meds/18fMedListController.js
+++ ui/app/modules/components/meds/18fMedListController.js
@@ -1,5 +1,9 @@
-angular.module('18f').controller('18fMedListController', function($scope, ProfileService) {
+angular.module('18f').controller('18fMedListController', function($scope, $location, ProfileService) {
     'use strict';
+
+    $scope.goTo = function(medId) {
+        $location.path('/meds/' + medId);
+    };
 
     var initialize = function() {
         $scope.ProfileService = ProfileService;
